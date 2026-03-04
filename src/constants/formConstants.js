280157b9ef5e22d55378975c6a330f5161961f41
bhanukiran12 <bhanukiran750.com>
2026-03-04 18:31:08 +0530

    added segment and UUID

--- src/constants/formConstants.js
+++ src/constants/formConstants.js
@@ -3,6 +3,10 @@ export const OTP_SECONDS = 20;
 export const PARENT_WINDOW_ORIGIN = 'https://ccbp-4-0-technical-intensive-project.webflow.io';
 export const PARENT_PAGE_URL = 'https://ccbp-4-0-technical-intensive-project.webflow.io/new-intensive';
 export const TARGET_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1mYoWaXaJUY9cYVDxGy7lSWAWIwmfsDIjIziefAsYKeA/edit?usp=sharing';
+export const DRAFT_USER_API_URL = 'https://ib-user-accounts-backend-gamma-apis.ccbp.in/api/nxtwave_clients/user/account/draft/v1/';
+export const DRAFT_USER_API_KEY = 'aX6TI0JV.GD0Bz43ntlBHsRZAqFBeGE0zB0SdRWqh';
+export const SEGMENT_TRACK_URL = 'https://api.segment.io/v1/track';
+export const SEGMENT_WRITE_KEY = 'pSF1EDPoC6XzbKeiyn2N3StqrlGdPHFT';
 
 export const GRAD_YEARS_ONLINE = ['2026', '2025', '2024', '2023', '2022', '2021', '2020'];
 export const GRAD_YEARS_CLASSROOM = ['2026', '2025', '2024'];
